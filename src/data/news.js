3c68b30109c62ea68eaf35d93b81083b3a188f6c
duydonche2002 <phananhduy2710@gmail.com>
2024-01-11 15:59:02 +0700

    nghịch lần 3

--- src/data/news.js
+++ src/data/news.js
@@ -5,9 +5,14 @@ export const news = [
     img:'https://img.iproperty.com.my/angel/520x300-crop/wp-content/uploads/sites/7/2024/01/1.jpg'
     },
     {
-    title:'Chính Sách Tốt Nhất Thị Trường Công Bố Tại Sự Kiện Mở Bán Happy One Central',
-    summary:'Chủ đầu tư dự án “căn hộ tốt nhất Bình Dương” Happy One Central đã chính thức công bố loạt chính sách chưa tiền lệ ngay tại sự kiện mở bán ngày 07/01 vừa qua, mở ra cơ hội sở hữu căn hộ trung tâm cho hàng', 
-    date:'10/01/2024 08:00',
+    title:'Lộ Diện Khu Vực Sẽ Hưởng Lợi Lớn Khi Thành Phố Phía Tây Thủ Đô Hình Thành',
+    summary:'Trong 5 năm tới sự phát triển của thị trường bất động sản Hà Nội sẽ lấy phía Tây là trọng điểm, khi thành phố phía Tây dần hình thành sẽ tạo bước ngoặt tăng trưởng cho vùng thủ đô, đặc biệt là khu vực Hòa Bình vốn ôm',
+    img:'https://img.iproperty.com.my/angel/520x300-crop/wp-content/uploads/sites/7/2024/01/1.jpg'
+    },
+    {
+    title:'Bất Động Sản 2024: Nhiều Kế Hoạch Mới Đang Vào Guồng',
+    summary:'Thị trường bất động sản 2024 dự báo sẽ chuyển mình với bức tranh “ấm” hơn, nhiều doanh nghiệp đặt ra chiến lượt phát triển mới.', 
+    date:'09/01/2024 08:11',
     img:'https://img.iproperty.com.my/angel/520x300-crop/wp-content/uploads/sites/7/2024/01/1.jpg'
     },
 ]
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -2,21 +2,21 @@ import { news } from "../data/news";
 
 function News() {
   return (
-    <div className=" display flex m-auto">
-      <div className=" display block m-auto p-[20px] ">
+    <div className=" display flex m-auto bg-white">
+      <div className=" display block m-auto p-[20px] bg-white ">
         {news.map((item) => (
-          <div className="display flex h-[150px] max-w-[800px] bg-white ">
+          <div className="p-[10px] display flex h-[150px] max-w-[800px] bg-white ">
             <img src={item.img} className="" />
-            <div className="p-[10px]">
+            <div className="pl-[10px]">
               <i>{item.date}</i>
-              <h1>{item.title}</h1>
+              <h1 className="text-xl">{item.title}</h1>
               <p>{item.summary}</p>
             </div>
           </div>
         ))}
       </div>
       <div className="display block">
-        <p>hi</p>
+        <p></p>
       </div>
     </div>
   );
