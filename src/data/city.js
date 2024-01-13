2db79b6ef658acf93efac8d5ba0b2facd2dbc708
duydonche2002 <phananhduy2710@gmail.com>
2024-01-13 13:17:37 +0700

    anh phan

---
+++ src/data/city.js
@@ -0,0 +1,10 @@
+export const city = [
+{
+    cityname:'Hà Nội',
+    img:'https://vcdn1-dulich.vnecdn.net/2022/05/12/Hanoi2-1652338755-3632-1652338809.jpg?w=0&h=0&q=100&dpr=2&fit=crop&s=NxMN93PTvOTnHNryMx3xJw'
+},
+{
+    cityname:'TP Hồ Chí Minh',
+    img:'https://tphcm.cdnchinhphu.vn/Uploaded/tranthithom/2017_07_06/TPHCM.jpg'
+}
+]
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,22 +1,66 @@
 import { news } from "../data/news";
+import { city } from "../data/city";
+import { navLinks } from "../constants/navlink";
+import { FaHouseChimney } from "react-icons/fa6";
+import { IoIosArrowForward } from "react-icons/io";
 
 function News() {
   return (
-    <div className=" display flex m-auto bg-white">
-      <div className=" display block m-auto p-[20px] bg-white ">
-        {news.map((item) => (
-          <div className="p-[10px] display flex h-[150px] max-w-[800px] bg-white ">
-            <img src={item.img} className="" />
-            <div className="pl-[10px]">
-              <i>{item.date}</i>
-              <h1 className="text-xl">{item.title}</h1>
-              <p>{item.summary}</p>
+    <div>
+      <div className="pt-[20px] pb-[20px] bg-white border-b flex">
+      <div className="flex xl:pl-[50px]"><FaHouseChimney /><IoIosArrowForward /></div>
+      <p>Tin tức</p>
+      </div>
+      <div className="m-auto block  bg-white  xl:flex ">
+        <div className=" m-auto block  bg-white  ">
+          {news.map((item) => (
+            <div className="flex h-[160px] max-w-[1000px] border-b-[1px] bg-white pb-[20px] pt-[20px] xl:max-w-[800px] ">
+              <img src={item.img} className="rounded-lg" />
+              <div className="pl-[10px]">
+                <i className="text-gray-400">{item.date}</i>
+                <h1 className="lg:text-lg text-sm">{item.title}</h1>
+                <p className=" line-clamp-2 lg:text-sm text-xs">{item.summary}</p>
+              </div>
+            </div>
+          ))}
+          <div className="p-[20px] text-center">
+            <button className="rounded-lg border border-black bg-white  px-4  py-2 text-black hover:bg-gray-400">
+              Xem thêm
+            </button>
+          </div>
+        </div>
+        <div className="block pb-[20px] pr-[0px] pt-[100px] xl:pr-[50px] ">
+          <div className="block max-w-[1000] rounded-lg  bg-slate-300 p-[20px] xl:max-w-[250px] ">
+            <h1 className="text-center text-xl">Tin nổi bật</h1>
+            {news.map((item) => (
+              <div className="border-b-[1px] pb-[10px] pt-[10px]">
+                <i className="text-gray-400">{item.date}</i>
+                <h1 className="text-base">{item.title}</h1>
+              </div>
+            ))}
+          </div>
+          <div className="block pt-[20px]">
+            <div className="block max-w-[1000] rounded-lg  bg-slate-200 p-[20px] xl:max-w-[250px] ">
+              <h1>Thị trường BDS tại tỉnh/thành phố</h1>
+              {city.map((item) => (
+                <div className="m-auto flex pt-[20px]">
+                  <img src={item.img} className="w-[40px] rounded-lg" />
+                  <h1 className="pl-[10px]">{item.cityname}</h1>
+                </div>
+              ))}
             </div>
           </div>
-        ))}
+        </div>
       </div>
-      <div className="display block">
-        <p></p>
+      <div className="flex w-full items-center justify-center bg-white">
+        {navLinks.map((item) => (
+          <div key={item.title} className="">
+            <h1>{item.title}</h1>
+            {item.child_links.map((item2)=>(
+            <p>{item2.title}</p>
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   );
